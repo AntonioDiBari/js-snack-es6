f655b7f42bd5788ba4de1e9e9583283c30784fe5
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-25 16:30:29 +0100

    feat: Snack 4

--- SNACK 4/README.md
+++ SNACK 4/README.md
@@ -13,3 +13,8 @@ Usa l'array allegato alla traccia per completare i seguenti punti:
    Stampare in pagina oltre che in console!
 
 ## SVOLGIMENTO
+
+- Inizializzato l'Array di oggetti lo scorro e per ogni oggetto creo un numero randomico per punteggio e falli subiti
+- Creo un Array vuoto da valorizzare
+- Scorro l'Array iniziale, lo destrutturo per ottenere nome e falli subiti e li aggiungo man mano al nuovo Array sotto forma di Obj.
+- Stampo quindi via via anche il suo contenuto
--- SNACK 4/js/main.js
+++ SNACK 4/js/main.js
@@ -35,3 +35,34 @@ const teams = [
     foul: 0,
   },
 ];
+
+const arrayOutput = document.getElementById("list");
+let result = document.getElementById("result");
+
+// PER OIGNI OBJ GENERO IL NUMERO RANDOMICO PER PUNTEGGIO E FALLI SUBITI
+teams.forEach((team) => {
+  team.score = generateNumber(1, 114);
+  team.foul = generateNumber(1, 400);
+  arrayOutput.innerHTML += `<li> <i class="fw-bold">${team.name} </i><ul><li>Ha totalizzato un totale di ${team.score} punti subendo in totale ${team.foul} falli nella stagione.</li></ul></li>`;
+});
+
+console.table(teams);
+
+// CREAO IL NUOVO ARRAY
+let teamsInfo = [];
+let teamsInfo2 = [];
+
+// CREO L'ELEMENTO OBJ CON SOLO NOME E FALLI SUBITI, LI AGGIUNGO QUINDI ALL'ARRAY SOTTO FORMA DI OBJ
+for (let team of teams) {
+  let { name, foul } = team;
+  console.log(name, foul);
+  teamsInfo.push({ name, foul });
+  result.innerHTML += `La squadra <span class="fw-bold">${name}</span> ha subito un tot. di <span class="text-danger">${foul}</span> falli nell'arco della stagione </br>`;
+  console.table(
+    `La squadra ${name} ha subito un tot. di ${foul} falli nell'arco della stagione`
+  );
+}
+
+// OPPPURE NE CREO UNO MAPPANDO QUELLO INIZIALE E GENERO UN ARRAY DI STRING
+teamsInfo2 = teams.map(({ name, foul }) => `${name} + ${foul}`);
+console.log(teamsInfo2);
